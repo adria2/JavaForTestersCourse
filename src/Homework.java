3c169c49ab5a836932dbebf2530c1fd6ca0728f9
Adrianna Mendalka <a.mendalka.865@studms.ug.edu.pl>
2022-07-09 12:19:28 +0200

    Added first homework file

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,34 @@
 public class Homework {
     public static void main(String[] args) {
-        System.out.println("Homework class");
+
+        // 1. Cztery zmienne typu int
+        double firstNumber = 23;
+        double secondNumber = 45;
+        double thirdNumber = 66;
+        double fourthNumber = 8;
+//        double sumFirstAndSecond = firstNumber + secondNumber;
+//        double sumThirdAndForth = thirdNumber + fourthNumber;
+
+
+        // 2. Sprawdzenie czy suma dwóch pierwszych jest parzysta
+        if ((firstNumber + secondNumber) % 2 == 0) {
+            System.out.println("Suma pierwszej i drugiej liczby jest parzysta");
+        } else {
+            System.out.println("Suma pierwszej i drugiej liczby jest nieparzysta");
+        }
+
+        // 3. Sprawdzenie czy suma dwóch pozostałych jest parzysta
+        if ((thirdNumber + fourthNumber) % 2 == 0) {
+            System.out.println("Suma trzeciej i czwartej liczby jest parzysta");
+        } else {
+            System.out.println("Suma trzeciej i czwartej liczby jest nieparzysta");
+        }
+
+        // 4. Jeżeli dwie sumy są parzyste to wyświetl informację, że obie liczby są parzyste
+        if (((firstNumber + secondNumber) % 2 == 0) && ((thirdNumber + fourthNumber) % 2 == 0)) {
+            System.out.println("Obie sumy są parzyste");
+        } else {
+            System.out.println("Przynajmniej jedna z sum jest nieparzysta");
+        }
     }
 }
